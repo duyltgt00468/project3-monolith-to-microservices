db30691294a796148200c116172de6c34c8bb5d8
Luu The Duy <duy.luu@coxautoinc.com>
2022-07-17 11:51:12 +0700

    master | update code base

---
+++ udagram-api-feed/set_env.sh
@@ -0,0 +1,11 @@
+# This file is used for convenience of local development.
+# DO NOT STORE YOUR CREDENTIALS INTO GIT
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=luutheduy
+export POSTGRES_HOST=mydbpro3.c1ql7fgcgter.us-east-1.rds.amazonaws.com
+export POSTGRES_DB=postgres
+export AWS_BUCKET=project3-196005-dev
+export AWS_REGION=us-east-1
+export AWS_PROFILE=default
+export JWT_SECRET=testing
+export URL=http://localhost:8100
